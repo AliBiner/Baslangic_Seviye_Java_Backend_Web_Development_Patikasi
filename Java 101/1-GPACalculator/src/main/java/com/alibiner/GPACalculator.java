6c5b2789a4ad2bcd13de87c1a09f7f11b1355d62
Ali Biner <alibiner.github@gmail.com>
2025-07-10 13:06:30 +0300

    add GPA Calculator Project

---
+++ Java 101/1-GPACalculator/src/main/java/com/alibiner/GPACalculator.java
@@ -0,0 +1,14 @@
+package com.alibiner;
+
+import java.util.List;
+
+public class GPACalculator {
+    // This method calculates average score of lessons
+    public static float averageCalculator(List<Lesson> lessons){
+        int scoreSum = 0;
+        for(Lesson lesson : lessons){
+            scoreSum += lesson.getScore(); // add all scores
+        }
+        return (float) scoreSum / lessons.size(); // return average
+    }
+}
