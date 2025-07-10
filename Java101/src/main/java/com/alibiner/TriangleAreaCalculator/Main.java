fd33d366a7c8be9380fc80d60899b69e14ea2fc5
Ali Biner <alibiner.github@gmail.com>
2025-07-10 13:35:54 +0300

    add TriangleAreaCalculator Project

---
+++ Java101/src/main/java/com/alibiner/TriangleAreaCalculator/Main.java
@@ -0,0 +1,9 @@
+package com.alibiner.TriangleAreaCalculator;
+
+
+public class Main {
+    public static void main(String[] args) {
+        TriangleCalculator triangleCalculator = new TriangleCalculator();
+        triangleCalculator.start();
+    }
+}
