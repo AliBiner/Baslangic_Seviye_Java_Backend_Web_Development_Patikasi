bdadee20dcc400a780a42d28077b47c93abf7c29
Ali Biner <alibiner.github@gmail.com>
2025-07-13 22:51:50 +0300

    create Teacher class for "Student Information System" project

---
+++ Java101/src/main/java/com/alibiner/StudentInformationSystem/Teacher.java
@@ -0,0 +1,11 @@
+package com.alibiner.StudentInformationSystem;
+
+public class Teacher {
+    String name, mpno, brach;
+
+    public Teacher(String name, String mpno, String brach) {
+        this.name = name;
+        this.mpno = mpno;
+        this.brach = brach;
+    }
+}
